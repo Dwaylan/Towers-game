2b9c751d2006aaf587d5ee91f5fe1dcb07e39e86
Dwaylan <Dwaylan.Applewhite@gmail.com>
2020-08-24 17:53:24 -0500

    added the notes to decipher what my code SHOULD do

--- main.js
+++ main.js
@@ -22,12 +22,13 @@ output: process.stdout
         // * 1 is the smallest
 
 
-// very similar to the board of arrays we used for Tic-Tac-Toe
-// we use let because the variable is expected to change
+// very similar to the board of arrays we used for Tic-Tac-Toe.
+// we use "let" because the variable is expected to change
+// The board is an object and  a, b, and c are keys that hold arrays.
 let stacks = {
 a: [4, 3, 2, 1],
 b: [],
-c: []
+c: [],
 };
 
 // Start here. What is this function doing?
@@ -35,27 +36,44 @@ const printStacks = () => {
 console.log("a: " + stacks.a);
 console.log("b: " + stacks.b);
 console.log("c: " + stacks.c);
+// printStacks is literally printing/logging the stacks inside of the terminal
+// on top of each other like the board
+// It prints the key as text and corresponding array right next to it
 }
 
 // Next, what do you think this function should do?
-const movePiece = () => {
-  // Your code here
+// movePiece should have a start and an end. The game should start with the startStack
+// aka "a"
+// and continue on until the proper sequence is reached at the end stack.
+// aka stack "c"
+const movePiece = (startStack, endStack) => {
+// function "movePiece" takes startStack and endStack as arguemts
 
 }
 
-// Before you move, should you check if the move it actually allowed? Should 3 be able to be stacked on 2
-const isLegal = () => {
+
+// Before you move, should you check if the move it actually allowed? 
+// Should 3 be able to be stacked on 2?
+// This function is to check and see if the top piece from the startStack to the top piece
+// of the endStack is a legal move. Remember, larger numbers CANNOT be stacked on smaller numbers
+const isLegal = (startStack, endStack) => {
   // Your code here
 
 }
 
 // What is a win in Towers of Hanoi? When should this function run?
-const checkForWin = () => {
+// "win" is when endStack is equal to: [4,3,2,1]
+// *** note that the endStack can be "b" OR "c". It's not necessarily "c"
+// just because "c" is the last array.***
+const checkForWin = (startStack, endStack) => {
   // Your code here
 
 }
 
 // When is this function called? What should it do with its argument?
+// This is the one function that runs the game
+// startStack is what the user enters for their starting stack
+// endStack is what the user enters for their ending stack
 const towersOfHanoi = (startStack, endStack) => {
   // Your code here
 
